b5fc434bbdfdaa6e62601ab5645a970b6c3d02d0
Jean-Luc Chassaing <jlchassaing@gmail.com>
2018-11-27 07:54:00 +0100

    add toolbar controller

--- Gie/EzToolbarBundle/Controller/ToolbarController.php
+++ Gie/EzToolbarBundle/Controller/ToolbarController.php
@@ -2,25 +2,254 @@
 
 namespace Gie\EzToolbarBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EzSystems\EzPlatformAdminUi\Form\Data\Content\Draft\ContentCreateData;
+use EzSystems\EzPlatformAdminUi\Form\SubmitHandler;
+use EzSystems\EzPlatformFormBuilder\Form\Type\Field\HiddenFieldType;
+use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use eZ\Publish\API\Repository\ContentService;
+use eZ\Publish\API\Repository\ContentTypeService;
+use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\Values\Content\Location;
+use eZ\Publish\API\Repository\Values\ContentType\ContentType;
+use eZ\Publish\API\Repository\PermissionResolver;
+use eZ\Publish\Core\MVC\Symfony\Templating\GlobalHelper;
+use Gie\EzToolbarBundle\Toolbar\Action;
+use EzSystems\EzPlatformAdminUi\Form\Factory\FormFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use eZ\Publish\Core\MVC\Symfony\Security\Authorization\Attribute ;
+use Symfony\Component\Templating\EngineInterface;
 
 class ToolbarController extends Controller
 {
 
-    public function renderAction(Request $request)
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @var ContentTypeService
+     */
+    private $contentTypeService;
+
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var LocationService
+     */
+    private $locationService;
+
+    /**
+     * @var PermissionResolver
+     */
+    private $permissionResolver;
+
+    /**
+     * @var SubmitHandler
+     */
+    private $submitHandler;
+
+    /**
+     * @var FormFactory
+     */
+    private $formFactory;
+
+    /**
+     * @var GlobalHelper
+     */
+    private $globalHelper;
+
+    /**
+     * @var array
+     */
+    private $languages;
+
+
+    /**
+     * ToolbarController constructor.
+     *
+     * @param EngineInterface $templating
+     * @param ContentTypeService $contentTypeService
+     * @param ContentService $contentService
+     * @param LocationService $locationService
+     * @param PermissionResolver $permissionResolver
+     * @param FormFactory $formFactory
+     * @param GlobalHelper $globalHelper
+     * @param array $languages
+     */
+    public function __construct(
+        EngineInterface $templating,
+        ContentTypeService $contentTypeService,
+        ContentService $contentService,
+        LocationService $locationService,
+        PermissionResolver $permissionResolver,
+        SubmitHandler $submitHandler,
+        FormFactory $formFactory,
+        GlobalHelper $globalHelper,
+        array $languages
+    )
+    {
+        $this->templating = $templating;
+        $this->contentTypeService = $contentTypeService;
+        $this->contentService = $contentService;
+        $this->locationService = $locationService;
+        $this->permissionResolver = $permissionResolver;
+        $this->submitHandler = $submitHandler;
+        $this->formFactory = $formFactory;
+        $this->globalHelper = $globalHelper;
+        $this->languages = $languages;
+    }
+
+
+    public function renderAction(Request $request, $locationId = null)
     {
         $response = new Response();
-        if ($this->isGranted(new Attribute('toolbar', 'use')))
+        $parentLocation = $this->getCurrentLocation($locationId);
+
+        if ($this->permissionResolver->hasAccess('toolbar', 'use'))
         {
-            return $this->render("@GieEzToolbar/toolbar/toolbar.html.twig",
-                [],
-                $response);
+            $contentTypes = $this->getCanPublishContentTypes($parentLocation);
+
+            $form = $this->formFactory->createContent();
+
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $result = $this->submitHandler->handle($form, function (ContentCreateData $data) {
+                    $contentType = $data->getContentType();
+                    $language = $data->getLanguage();
+                    $parentLocation = $data->getParentLocation();
+
+
+                    return $this->redirectToRoute('ez_content_create_no_draft', [
+                        'contentTypeIdentifier' => $contentType->identifier,
+                        'language' => $language->languageCode,
+                        'parentLocationId' => $parentLocation->id,
+                    ]);
+                });
+
+                if ($result instanceof Response) {
+                    return $result;
+                }
+            }
+
+            $response->setContent( $this->templating->render("@GieEzToolbar/toolbar/toolbar.html.twig",
+                ['form' => $form->createView()]));
         }
-        else{
-            return $response;
+        return $response;
+    }
+
+    public function actionAction(Request $request)
+    {
+        $response = new Response();
+
+
+        if ($this->permissionResolver->hasAccess('toolbar', 'use'))
+        {
+
+            $form = $this->formFactory->createContent();
+
+            if ( $form->isSubmitted() && $form->isValid() )
+            {
+                $result = $this->submitHandler->handle( $form, function ( ContentCreateData $data ) {
+                    $contentType    = $data->getContentType();
+                    $language       = $data->getLanguage();
+                    $parentLocation = $data->getParentLocation();
+
+
+                    return $this->redirectToRoute( 'ez_content_create_no_draft', [
+                        'contentTypeIdentifier' => $contentType->identifier,
+                        'language'              => $language->languageCode,
+                        'parentLocationId'      => $parentLocation->id,
+                    ] );
+                } );
+
+                if ( $result instanceof Response )
+                {
+                    return $result;
+                }
+            }
+            else
+            {
+                $response->setContent( $this->templating->render("@GieEzToolbar/toolbar/toolbar.html.twig",
+                    ['form' => $form->createView()]));
+                return $response;
+            }
+
         }
     }
+
+    /**
+     * @param Location $parentLocaiton
+     *
+     * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    protected function getCanPublishContentTypes(Location $parentLocation)
+    {
+        $canPublishContentType = [];
+        $contentTypeGroups = $this->contentTypeService->loadContentTypeGroups();
+        foreach ( $contentTypeGroups as $contentTypeGroup )
+        {
+            $contentTypes = $this->contentTypeService->loadContentTypes($contentTypeGroup);
+
+            foreach ( $contentTypes as $contentType )
+            {
+                if( $this->canPublish($contentType, $parentLocation, $this->languages[0]))
+                {
+                    $canPublishContentType[$contentType->getName()] = $contentType->id;
+                }
+            }
+        }
+        return $canPublishContentType;
+    }
+
+    /**
+     * @param ContentType $contentType
+     * @param Location $location
+     * @param $language
+     *
+     * @return bool
+     * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    protected function canPublish(ContentType $contentType,Location $location, $language)
+    {
+        $contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, $language);
+        $locationCreateStruct = $this->locationService->newLocationCreateStruct($location->id);
+
+        return $this->permissionResolver->canUser('content', 'publish', $contentCreateStruct, [$locationCreateStruct]);
+
+    }
+
+
+    /**
+     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     */
+    protected function getRootLocation()
+    {
+        return $this->globalHelper->getRootLocation();
+    }
+
+    /**
+     * @param null $locationId
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    protected function getCurrentLocation($locationId = null)
+    {
+        if ($locationId !== null && is_integer($locationId))
+        {
+            return $this->locationService->loadLocation($locationId);
+        }
+        else{
+            return $this->getRootLocation();
+        }
+    }
+
 }
---
+++ Gie/EzToolbarBundle/Toolbar/Action.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlchassaing
+ * Date: 11/11/2018
+ * Time: 22:36
+ */
+
+namespace Gie\EzToolbarBundle\Toolbar;
+
+
+class Action
+{
+
+    /** @var  */
+    public $contentTypeId;
+
+}
